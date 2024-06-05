9104e23952516fbc285f24a1ee642afb171ba6ff
Vikhan Muharram <127295441+vikhanmuhammad@users.noreply.github.com>
2024-06-05 16:26:19 +0700

    Update AppTest.java

--- src/test/java/coba/AppTest.java
+++ src/test/java/coba/AppTest.java
@@ -1,12 +1,10 @@
 package src.test.java.coba;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.Assert.assertTrue;
 import src.main.java.coba.TanggalHelper;
 import org.junit.Test;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Unit test for simple App.
